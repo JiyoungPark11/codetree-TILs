51f53f0a6754dd997868033f907806f8211672e2
Ji <151882150+JiyoungPark11@users.noreply.github.com>
2024-07-08 21:40:10 +0900

    Title: 장학금, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/장학금/scholarship.py
+++ 240708/장학금/scholarship.py
@@ -4,9 +4,9 @@ b = int(i[1])
 
 if a >= 90 :
     if 95 <= b < 100 :
-        print("10")
+        print("100000")
     elif b >= 90 :
-        print("5")
+        print("50000")
     else :
         print("0")
 else :
