a0412948d7a9d3a6575659e6c133b2e9ed5947ce
Ji <151882150+JiyoungPark11@users.noreply.github.com>
2024-07-08 21:12:25 +0900

    Title: 비교 연산, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240708/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+i = input().split()
+a = int(i[0])
+b = int(i[1])
+
+if a >= b :
+    print("1")
+else :
+    print("0")
+
+if a > b :
+    print("1")
+else :
+    print("0")
+
+if a <= b :
+    print("1")
+else :
+    print("0")
+
+if a < b :
+    print("1")
+else :
+    print("0")
+
+if a == b :
+    print("1")
+else :
+    print("0")
+
+if a != b :
+    print("1")
+else :
+    print("0")
