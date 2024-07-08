5df344a029dad3e474bc68a6d58dce8227ad55fe
Ji <151882150+JiyoungPark11@users.noreply.github.com>
2024-07-08 21:25:33 +0900

    Title: 특정 조건 세 정수 비교, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240708/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,14 @@
+i = input().split()
+a = int(i[0])
+b = int(i[1])
+c = int(i[2])
+
+if a <= b and a <= c :
+    print("1",  end = " ")
+else :
+    print("0", end = " ")
+
+if a == b and b == c :
+    print("1")
+else :
+    print("0")
