7dbfc634dfb8b9abe1f9b290fc055ee2ae95db96
Ji <151882150+JiyoungPark11@users.noreply.github.com>
2025-06-18 13:24:07 +0900

    Title: 청소당번 정하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250618/청소당번 정하기/cleaning-numbering.py
+++ 250618/청소당번 정하기/cleaning-numbering.py
@@ -3,20 +3,12 @@ cls = 0
 hall = 0
 toilet = 0
 
-for i in range(1, n):
-    if i % 2 == 0:
-        if i % 3 == 0 :
-            hall += 1
-        elif i % 12 == 0:
-            toilet += 1
-        else:
-            cls += 1
-    elif i % 3 == 0:
-        if i % 12 == 0:
-            toilet += 1
-        else:
-            hall += 1
-    elif i % 12 == 0:
+for i in range(1, n+1):
+    if i % 12 == 0:
         toilet += 1
+    elif i % 3 == 0:
+        hall += 1
+    elif i % 2 == 0:
+        cls += 1
 
 print(cls, hall, toilet)
