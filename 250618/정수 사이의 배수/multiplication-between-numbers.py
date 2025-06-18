7911f84302f38360b9d26919bebace6518bb5ed4
Ji <151882150+JiyoungPark11@users.noreply.github.com>
2025-06-18 13:55:38 +0900

    Title: 정수 사이의 배수, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250618/정수 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,14 @@
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+sum_val = 0
+item_num = 0
+
+for i in range(a, b+1):
+    if i % 5 == 0 or i % 7 == 0:
+        sum_val += i
+        item_num += 1
+
+avg = round(sum_val / item_num)
+print(sum_val, avg)
